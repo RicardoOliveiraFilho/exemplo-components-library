e79dadc98105cb46bcb71b5d62c6c5a7e841bcd6
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-11-11 15:26:02 -0300

    fixing some others problems

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import { terser } from 'rollup-plugin-terser';
 
 const packageJson = require("./package.json");
 
@@ -18,6 +19,15 @@ export default {
       file: packageJson.module,
       format: "esm",
       sourcemap: true
+    },
+    {
+      file: 'dist/components.min.js',
+      format: 'iife',
+      name: 'version',
+      globals: {
+        'react': 'React',
+      },
+      plugins: [terser()]
     }
   ],
   plugins: [
